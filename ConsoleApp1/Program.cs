722aeb8aeb87b37b672f16789e6f255368869aff
ZaraGiraffe <znaumets@gmail.com>
2022-10-07 15:34:37 +0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿abstract class Географічний_обєкт
+{
+    public int x;
+    public int y;
+    public string назва;
+    public string опис;
+    public virtual void отримати_інформацію()
+    {
+        
+    }
+}
+
+class Річка : Географічний_обєкт
+{
+    public int швидкість_течії;
+    public int загальна_довжина;
+    
+}
+
+class Гора : Географічний_обєкт
+{
+    public int найвища_точка;
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,83 @@
+﻿interface Географічний_обєкт
+{
+    int x {get; set;}
+    int y {get; set;}
+    string назва {get; set;}
+    string опис {get; set;}
+    virtual void отримати_інформацію()
+    {
+           
+    }
+}
+
+class Річка : Географічний_обєкт
+{
+    public int швидкість_течії;
+    public int загальна_довжина;
+    public int _x;
+    public int _y;
+    public string _назва;
+    public string _опис;
+    public int x
+    {
+        get => _x; 
+        set => _x = value; 
+    }
+    public int y
+    {
+        get => _y;
+        set => _y = value;
+    }
+    public string назва
+    {
+        get => _назва;
+        set => _назва = value;
+    }
+    
+    public string опис
+    {
+        get => _опис; 
+        set => _опис = value;
+    }
+}
+
+class Гора : Географічний_обєкт
+{
+    private int найвища_точка;
+    public int _x;
+    public int _y;
+    public string _назва;
+    public string _опис;
+    public int x
+    {
+        get => _x; 
+        set => _x = value; 
+    }
+    public int y
+    {
+        get => _y;
+        set => _y = value;
+    }
+    public string назва
+    {
+        get => _назва;
+        set => _назва = value;
+    }
+    
+    public string опис
+    {
+        get => _опис; 
+        set => _опис = value;
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        Гора s = new();
+        s.x = 3;
+        Console.WriteLine("ooo");
+    }
+}
